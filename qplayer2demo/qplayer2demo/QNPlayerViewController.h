838ff45d272f5e038c36e4b8a3a53fc50b922b9c
Mengwangchao <www.903064748@qq.com>
2022-08-29 17:06:54 +0800

    新建工程

---
+++ qplayer2demo/qplayer2demo/QNPlayerViewController.h
@@ -0,0 +1,17 @@
+//
+//  QNPlayerViewController.h
+//  QPlayerKitDemo
+//
+//  Created by 冯文秀 on 2017/9/18.
+//  Copyright © 2017年 qiniu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "QNClassModel.h"
+
+@interface QNPlayerViewController : UIViewController
+
+@property (nonatomic, strong) NSArray<QNClassModel*> *playerConfigArray;
+
+
+@end
